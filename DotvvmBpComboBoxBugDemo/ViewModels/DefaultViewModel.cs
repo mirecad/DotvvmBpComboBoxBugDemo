c100d83bafc48074d19e1adce50672a8b1b95ad0
Miroslav Adamec <miroslav.adamec@invia.cz>
2023-03-02 14:31:31 +0100

    Initial commit

---
+++ DotvvmBpComboBoxBugDemo/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DotvvmBpComboBoxBugDemo.ViewModels;
+
+public class ComboBoxItemDto
+{
+    public int Id { get; set; }
+    public string Label { get; set; }
+}
+
+public class DefaultViewModel : MasterPageViewModel
+{
+    public ComboBoxItemDto SelectedItem { get; set; }
+
+    public List<ComboBoxItemDto> ComboBoxItems => new()
+    {
+        new ComboBoxItemDto()
+        {
+            Id = 1,
+            Label = "Item 1"
+        },
+        new ComboBoxItemDto()
+        {
+            Id = 2,
+            Label = "Item 1"
+        }
+    };
+}
