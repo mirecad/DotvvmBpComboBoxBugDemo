6a60857660dab51fa848475c19cdcfbc80f5aa5d
Miroslav Adamec <miroslav.adamec@invia.cz>
2023-03-02 14:59:24 +0100

    Exten combobox behavior

--- DotvvmBpComboBoxBugDemo/ViewModels/DefaultViewModel.cs
+++ DotvvmBpComboBoxBugDemo/ViewModels/DefaultViewModel.cs
@@ -6,6 +6,7 @@ public class ComboBoxItemDto
 {
     public int Id { get; set; }
     public string Label { get; set; }
+    public string Detail { get; set; }
 }
 
 public class DefaultViewModel : MasterPageViewModel
@@ -17,12 +18,14 @@ public class DefaultViewModel : MasterPageViewModel
         new ComboBoxItemDto()
         {
             Id = 1,
-            Label = "Item 1"
+            Label = "Item 1",
+            Detail = "Detail 1"
         },
         new ComboBoxItemDto()
         {
             Id = 2,
-            Label = "Item 1"
+            Label = "Item 1",
+            Detail = "Detail 2"
         }
     };
 }
